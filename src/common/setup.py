2e999658383aa72ecf6d31a0cc653ad25aa7264b
Hamish Songsmith <hamish_songsmith@icloud.com>
2022-07-31 20:05:40 +1000

    allow the config to update if the template updates

--- src/common/setup.py
+++ src/common/setup.py
@@ -23,28 +23,26 @@ def confirm_config() -> None:
     if os.path.isfile('./config.ini') is False:
         log.warning('./config.ini does not exist')
         create_config()
+        config_file = configparser.ConfigParser()
+        config_file.read('config.ini')
+    else:
+        config_file = configparser.ConfigParser()
+        config_file.read('config.ini')
+        template_file = configparser.ConfigParser()
+        template_file.read('config_template.ini')
 
-    # Import the file
-    config_file = configparser.ConfigParser()
-    config_file.read('config.ini')
+        config_file = update_config(config_file=config_file,
+                                    template_file=template_file)
 
-    # Parse though the sections to complete the missing values
-    for section in config_file.keys():
-        for key, value in config_file[section].items():
-            if len(value) == 0:
-                log.info(f"key [{key}] is missing; attempting to capture")
-                if key != 'password':
-                    input_value = input(f"{key}: ")
-                    log.info(f"Input: {input_value}")
-                else:
-                    input_value = getpass.getpass('password: ')
-                    log.info(f"Password Length: {len(input_value)}")
-
-                config_file.set(section, key, input_value)
-
-        # Save the changes to the config file
         with open('./config.ini', 'w') as configfile:
             config_file.write(configfile)
+
+    # Parse though the sections to complete the missing values
+    config_file = verify_values(config_file)
+
+    # Save the changes to the config file
+    with open('./config.ini', 'w') as configfile:
+        config_file.write(configfile)
 
     return config_file
 
@@ -61,8 +59,10 @@ def create_config() -> None:
 
 
 def update_config(config_file: configparser.ConfigParser,
-                  template_file: configparser.ConfigParser) -> None:
+                  template_file: configparser.ConfigParser) -> configparser.ConfigParser:
     '''Compares the config to the temaplte and creates missing keys'''
+
+    log.debug("CONFIG CHECK")
 
     # Get the config details
     config_sections = list(config_file.keys())
@@ -73,6 +73,7 @@ def update_config(config_file: configparser.ConfigParser,
         config_dict[key] = []
         for item in config_file[key].keys():
             config_dict[key].append(item)
+    log.debug(config_dict)
 
     # Get the template details
     template_sections = list(template_file.keys())
@@ -83,18 +84,24 @@ def update_config(config_file: configparser.ConfigParser,
         template_dict[key] = []
         for item in template_file[key].keys():
             template_dict[key].append(item)
+    log.debug(template_dict)
 
     for template_key, template_value in template_dict.items():
+        log.debug(f"CONFIG CHECKING {template_key}")
         if template_key not in config_dict.keys():
-            # Create the new key and attributes
-            pass
-        else:
+            log.info(f"CONFIG [{template_key}] NOT FOUND")
+            config_file.add_section(template_key)
             for template_attribute in template_value:
-                if template_attribute not in config_dict[key]:
-                    # Create the ne attribute
-                    pass
+                config_file.set(template_key, template_attribute, "")
+        else:
+            log.debug(f"CONFIG [{template_key}] FOUND")
+            for template_attribute in template_value:
+                if template_attribute not in config_dict[template_key]:
+                    log.debug(f"{template_key} {config_dict[key]}")
+                    log.info(f"CONFIG [{template_key}][{template_attribute}] NOT FOUND")
+                    config_file.set(template_key, template_attribute, "")
 
-    return
+    return config_file
 
 
 def get_environment() -> str:
@@ -102,3 +109,23 @@ def get_environment() -> str:
     response = pygit2.Repository('.').head.shorthand
 
     return response
+
+
+def verify_values(config: configparser.ConfigParser) -> configparser.ConfigParser:
+    ''''''
+    for section in config.keys():
+        for key, value in config[section].items():
+            if len(value) == 0:
+                log.info(f"key [{key}] is missing; attempting to capture")
+                if key == 'password':
+                    input_value = getpass.getpass('password: ')
+                    log.info(f"Password Length: {len(input_value)}")
+                elif key == 'env':
+                    input_value = get_environment()
+                else:
+                    input_value = input(f"{key}: ")
+                    log.info(f"Input: {input_value}")
+
+                config.set(section, key, input_value)
+
+    return config
